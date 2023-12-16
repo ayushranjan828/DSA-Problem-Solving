c4dd6edf481a0afa531cfd878bcdc102ca946059
Ayush Ranjan <119885031+ayushranjan828@users.noreply.github.com>
2023-12-16 10:26:47 +0530

    Delete Array 1D/tempCodeRunnerFile.cpp

--- Array 1D/tempCodeRunnerFile.cpp
+++
@@ -1,2 +0,0 @@
-Check if we can partition the array into two subarrays with equal sum.
-    More formally, check that the prefix sum of a part of the array is equal to the suffix sum of rest of the array.
