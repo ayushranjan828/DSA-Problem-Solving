a83c616ab6dbfe3aa927efa19946e50bc464e846
Ayush Ranjan <119885031+ayushranjan828@users.noreply.github.com>
2023-12-26 18:00:15 +0530

    Create Print_address_of_variable.cpp

---
+++ Pointer/Print_address_of_variable.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x = 10, y = 20;
+    cout<< &x <<endl;   // Memory adrress of my pc 0x61ff0c
+    cout<< &y;  // Memory adrress of my pc 0x61ff08
+    return 0;
+}
